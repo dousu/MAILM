87bff3ccebed73a1c29be2f631a4333334215eb7
Hiroki Sudo <hiroki.sudo.hs@gmail.com>
2018-05-18 10:51:28 +0900

    daily

--- SOURCE/Semantics.cpp
+++ SOURCE/Semantics.cpp
@@ -775,6 +775,8 @@ void Semantics<T>::unique_unify(Element a, std::vector<Element> v_e) {
 		mapping[b.obj] = b_v;
 	}
 
+	std::cerr << "Unify to a(" << a.obj << ")" << std::endl;
+
 	//ルールの変更aを取り除いてouterを1減らす
 	std::vector<Element> b_in_res;
 	std::vector<int> outer_list;
