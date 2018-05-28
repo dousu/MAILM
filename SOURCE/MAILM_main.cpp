c275dce05918625ffce6a1394c1bd62821169c4d
Hiroki Sudo <hiroki.sudo.hs@gmail.com>
2018-05-29 02:15:32 +0900

    daily

--- SOURCE/MAILM_main.cpp
+++ SOURCE/MAILM_main.cpp
@@ -82,8 +82,7 @@ void output_data(
 			boost::filesystem::ofstream ofs(path);
 			ofs << data;
 		}else if(!result){
-			boost::filesystem::ofstream ofs_empty(empty_path);
-			boost::filesystem::copy_file(ofs_empty,path, error);
+			boost::filesystem::copy_file(empty_path,path);
 			boost::filesystem::ofstream ofs(path);
 			ofs << data;
 		}else {
