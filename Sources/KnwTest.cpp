45996c193715a5ad1b92309e274bff16fd95a473
Hiroki Sudo <hiroki.sudo.hs@gmail.com>
2018-12-28 10:31:24 +0900

    Update KnwTest.cpp

--- Sources/KnwTest.cpp
+++ Sources/KnwTest.cpp
@@ -7,10 +7,14 @@ int main(int arg, char **argv) {
   std::for_each(std::begin(v), std::end(v), [&](int n) {
     std::string str = Prefices::SYN + std::to_string(n);
     std::string str2 = "[" + std::to_string(n) + "]";
-    Reader::alias.insert(std::map<std::string, std::string>::value_type(str, str2));
-    Reader::conv_alias.insert(std::map<std::string, std::string>::value_type(str2, str));
-    Dictionary::symbol.insert(std::map<int, std::string>::value_type(n, str2));
-    Dictionary::conv_symbol.insert(std::map<std::string, int>::value_type(str2, n));
+    Reader::alias.insert(
+        std::map<std::string, std::string>::value_type(str, str2));
+    Reader::conv_alias.insert(
+        std::map<std::string, std::string>::value_type(str2, str));
+    Dictionary::symbol.insert(
+        std::map<int, std::string>::value_type(n, str2));
+    Dictionary::conv_symbol.insert(
+        std::map<std::string, int>::value_type(str2, n));
     Reader::conv_str[str] = n;
   });
   Symbol::conv_symbol = Reader::conv_alias;
@@ -24,17 +28,21 @@ int main(int arg, char **argv) {
   // auto it1 = std::begin(dic.conv_individual);
   // for (; it1 != std::end(dic.conv_individual); it1++)
   // {
-  //     std::cout << "\tstring: " << (*it1).first << "\tnumber: " << (*it1).second << std::endl;
+  //     std::cout << "\tstring: " << (*it1).first << "\tnumber: " <<
+  //     (*it1).second << std::endl;
   // }
   std::cout << "symbols" << std::endl;
   auto it2 = std::begin(Dictionary::conv_symbol);
   for (; it2 != std::end(Dictionary::conv_symbol); it2++) {
-    std::cout << "\tstring: " << (*it2).first << "\tnumber: " << (*it2).second << std::endl;
+    std::cout << "\tstring: " << (*it2).first
+              << "\tnumber: " << (*it2).second << std::endl;
   }
 
   // chunk1 test
-  std::vector<SymbolElement> s_el1{Symbol{1}, Symbol{2}, Symbol{3}}, s_el2{Symbol{1}, Symbol{4}, Symbol{3}};
-  // std::vector<MeaningElement> m_el1{Meaning{1}, Meaning{2}, Meaning{3}},
+  std::vector<SymbolElement> s_el1{Symbol{1}, Symbol{2}, Symbol{3}},
+      s_el2{Symbol{1}, Symbol{4}, Symbol{3}};
+  // std::vector<MeaningElement> m_el1{Meaning{1}, Meaning{2},
+  // Meaning{3}},
   //     m_el2{Meaning{1}, Meaning{2}, Meaning{2}};
   std::cout << "\n****************chunk1 test" << std::endl;
   buf = Rule(LeftNonterminal(Category{-1}, Meaning(AMean{0})), s_el1);
@@ -72,7 +80,8 @@ int main(int arg, char **argv) {
   std::vector<SymbolElement> s_el4{Symbol{4}};
   std::cout << "\n****************merge test" << std::endl;
   std::cout << "\n%%% previous" << std::endl;
-  buf = Rule(LeftNonterminal(Category{-3}, Meaning(AMean{-7})), s_el4);
+  buf =
+      Rule(LeftNonterminal(Category{-3}, Meaning(AMean{-7})), s_el4);
   kb.send_box(buf);
   kb.define(AMean{-7}, Conception());
   std::cout << kb.to_s() << std::endl;
@@ -87,7 +96,8 @@ int main(int arg, char **argv) {
   std::vector<SymbolElement> s_el5{Symbol{3}};
   std::cout << "\n****************replace test" << std::endl;
   std::cout << "\n%%% previous" << std::endl;
-  buf = Rule(LeftNonterminal(Category{-4}, Meaning(AMean{-8})), s_el5);
+  buf =
+      Rule(LeftNonterminal(Category{-4}, Meaning(AMean{-8})), s_el5);
   kb.send_box(buf);
   kb.define(AMean{-8}, Conception(Prefices::MES));
   std::cout << kb.to_s() << std::endl;
@@ -102,7 +112,7 @@ int main(int arg, char **argv) {
   std::cout << "\n****************unique test" << std::endl;
   std::cout << "\n%%% previous" << std::endl;
   std::cout << kb.to_s() << std::endl;
-  kb.consolidate();
+  kb.learning();
   std::cout << "\n%%% after" << std::endl;
   std::cout << kb.to_s() << std::endl;
   std::cout << kb.intention.mapping_to_s() << std::endl;
@@ -111,9 +121,11 @@ int main(int arg, char **argv) {
   Rule r3 = kb.at(2);
   std::cout << r2 << std::endl
             << r3 << std::endl
-            << std::boolalpha << (r2.get_external() == r3.get_external()) << std::noboolalpha << std::endl;
+            << std::boolalpha
+            << (r2.get_external() == r3.get_external())
+            << std::noboolalpha << std::endl;
 
-  std::cout << "\n****************consolidate test" << std::endl;
+  std::cout << "\n****************learning test" << std::endl;
   TransRules tr;
   kb.clear();
   kb.send_box(vec);
@@ -128,7 +140,7 @@ int main(int arg, char **argv) {
   kb.init_semantics_rules(tr);
   std::cout << "\n%%% previous" << std::endl;
   std::cout << kb.to_s() << std::endl;
-  kb.consolidate();
+  kb.learning();
   std::cout << "\n%%% after" << std::endl;
   std::cout << kb.to_s() << std::endl;
   std::cout << kb.intention.mapping_to_s() << std::endl;
